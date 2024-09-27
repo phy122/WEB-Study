9a95a161cc8a6108ea3f676ccef4368b9f52d0f0
phy122 <qkrghdud369@gmail.com>
2024-09-27 18:17:06 +0900

    2024.09.27

---
+++ JS/06_DOM/메뉴/js/세로_기본.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", (event) =>{
+    const mainMenuList = document.querySelectorAll('.main-menu > .menu-item')
+
+    mainMenuList.forEach(function(menu){
+        const submenu = menu.children[1]
+
+        // 마우스 올렸을 때 
+        menu.addEventListener("mouseover", () =>{
+            console.log(menu.children);
+            // 해당 메인 메뉴의 서브 메뉴
+            submenu.classList.add('active')
+
+        })
+
+        // 마우스 벗어날 때
+        menu.addEventListener("mouseout", () =>{
+            submenu.classList.remove('active')
+        })
+
+    })
+
+})
